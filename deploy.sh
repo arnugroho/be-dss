9284e2667ed6542e89a66d91779b19803ad2f574
arnugroho <nugroho.akbar.r@gmail.com>
2024-10-30 05:36:37 +0700

    deploy

--- deploy.sh
+++ deploy.sh
@@ -11,4 +11,4 @@ echo "Build .."
 echo "RUN .."
 docker stop beapi-dss
 docker rm beapi-dss
-docker run -d --name=beapi-dss --ip 172.17.0.5 -p 9898:8080 be-dss:0.0.1-SNAPSHOT
+docker run -d -it --restart=always --name=beapi-dss --ip 172.17.0.5 -p 9898:8080 be-dss:0.0.1-SNAPSHOT
