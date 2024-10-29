c5e2cc131715bf030d759d40fcac0e875f1eef4e
arnugroho <nugroho.akbar.r@gmail.com>
2024-10-30 05:35:31 +0700

    deploy

---
+++ deploy.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#
+# /**
+# *@Author Akbar Riyan Nugroho
+# */
+#
+
+echo "Build .."
+./gradlew bootBuildImage
+echo "RUN .."
+docker stop beapi-dss
+docker rm beapi-dss
+docker run -d --name=beapi-dss --ip 172.17.0.5 -p 9898:8080 be-dss:0.0.1-SNAPSHOT
